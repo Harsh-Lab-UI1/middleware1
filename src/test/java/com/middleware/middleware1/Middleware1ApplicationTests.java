77a7eaf4a52dc5b16ad8aa390bc7bba6ee34a433
Harsh-Lab-UI1 <harshtiwari8765@gmail.com>
2022-02-21 20:26:50 +0530

    second commit

--- src/test/java/com/middleware/middleware1/Middleware1ApplicationTests.java
+++ src/test/java/com/middleware/middleware1/Middleware1ApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Middleware1ApplicationTests {
+public class Middleware1ApplicationTests {
 
 	@Test
-	void contextLoads() {
+public  void contextLoads() {
 	}
 
 }
